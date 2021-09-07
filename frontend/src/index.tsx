3e1bb95cdde9988c450f349a6d07d6cb17839b1e
Gabriel <brgabriel.monteiro@gmail.com>
2021-09-06 22:35:21 -0300

    troubleshooting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import NavBar from 'components/navbar';
+
 
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    
   </React.StrictMode>,
   document.getElementById('root')
 );
